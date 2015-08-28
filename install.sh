22b660b27027de80aa63adfd4f88378a2ee24639
Thomas Cousin <thomas.cousin@me.com>
2015-08-28 22:37:32 +0200

    Update install.sh
    Updates to pip instilation

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 apt-get -y install wget
 apt-get -y install unzip
 apt-get -y install python
-apt-get -y install python-pip
+apt-get install python-setuptools
+easy_install pip
 pip install onetimepass
 wget https://github.com/Tomcuzz/ssh-totp/archive/master.zip
 unzip master.zip -d /bin/
