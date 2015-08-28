c3322c5e043fb4743c0ab674f40659dc242fa83c
Thomas Cousin <thomas.cousin@me.com>
2015-08-28 16:46:46 +0200

    Create install.sh
    Creation of login script

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get install unzip python python-pip
+pip install onetimepass
+wget https://github.com/Tomcuzz/ssh-totp/archive/master.zip
+unzip master.zip -d /bin/
+rm master.zip
+chmod +x /bin/ssh-totp-master/login
+echo "ForceCommand /bin/ssh-totp-master/login" >> /etc/ssh/sshd_config
+ForceCommand /bin/ssh-totp-master/login
+service ssh restart
